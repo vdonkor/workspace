d750cf56b5bfb308d6f7a30a57012b038212a1ee
vdonkor <vincent.donkor@gmail.com>
2022-10-06 22:39:45 -0400

    adding scrapper

---
+++ main.py
@@ -0,0 +1,53 @@
+import json
+import boto3
+import botocore.exceptions
+import logging
+
+log = logging.getLogger(__name__)
+
+ses = boto3.Session()
+
+
+class Scrapper:
+
+    def __init__(self, session, region="us-east-1"):
+        self.session = session
+        self.region = region
+        self.account_id = self.session.client("sts").get_caller_identity()["Account"]
+
+    def check_kms_usage(self):
+        keys = []
+        try:
+            client = self.session.client("kms", region_name=self.region)
+            paginator = client.get_paginator("list_aliases")
+            page_iter = paginator.paginate()
+            for page in page_iter:
+                keys.extend([item["AliasName"] for item in page["Aliases"] if item["AliasName"].split("/")[1] != "aws"])
+
+            return {
+                "account_id": self.account_id,
+                "resource": "kms",
+                "count": len(keys),
+                "keys": keys
+            }
+        except botocore.exceptions.ClientError as e:
+            log.error(e)
+            return {
+                "account_id": self.account_id,
+                "resource": "kms",
+                "count": 0,
+                "keys": []
+            }
+
+    def check_kinesis_usage(self):
+        client = self.session.client("kinesis", region_name=self.region)
+        paginator = client.get_paginator("list_streams")
+        page_iter = paginator.paginate()
+        for page in page_iter:
+            print(page)
+
+
+scrapper = Scrapper(ses)
+
+scrapper.check_kinesis_usage()
+# print(resource)
