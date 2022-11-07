de21bf49a6df7b043e8b941af947a21665023066
Vincent Donkor <vincent.donkor@gmail.com>
2022-11-06 19:18:51 -0500

    zip func

--- terraform/alarm/lambda.tf
+++ terraform/alarm/lambda.tf
@@ -1,25 +1,25 @@
 resource "aws_iam_role" "this" {
   assume_role_policy = data.aws_iam_policy_document.this.json
-  name               =  var.lambda_role_name
+  name               = var.lambda_role_name
 }
 resource "aws_iam_policy" "this" {
   policy = data.aws_iam_policy_document.log.json
-  name = "${var.lambda_role_name}-policy"
+  name   = "${var.lambda_role_name}-policy"
 }
 resource "aws_iam_policy_attachment" "this" {
-  name = "${var.lambda_role_name}-attachement"
+  name       = "${var.lambda_role_name}-attachement"
   policy_arn = aws_iam_policy.this.arn
-  roles = [aws_iam_role.this.name]
+  roles      = [aws_iam_role.this.name]
 }
 resource "aws_lambda_function" "this" {
-  filename = data.archive_file.this.output_path
-  function_name = var.lambda_function_name
-  role = aws_iam_role.this.arn
-  handler = "main.handler"
-  memory_size = 128
-  timeout = 60
+  filename         = data.archive_file.this.output_path
+  function_name    = var.lambda_function_name
+  role             = aws_iam_role.this.arn
+  handler          = "main.handler"
+  memory_size      = 128
+  timeout          = 60
   source_code_hash = data.archive_file.this.output_base64sha256
-  runtime = "python3.8"
+  runtime          = "python3.8"
   environment {
     variables = {
       SLACK_URL     = var.slack_endpoint_url
@@ -29,14 +29,14 @@ resource "aws_lambda_function" "this" {
 }
 
 resource "aws_lambda_function" "filter" {
-  filename = data.archive_file.filter.output_path
-  function_name = var.log_filter_lambda_function_name
-  role = aws_iam_role.this.arn
-  handler = "main.handler"
-  memory_size = 128
-  timeout = 60
+  filename         = data.archive_file.filter.output_path
+  function_name    = var.log_filter_lambda_function_name
+  role             = aws_iam_role.this.arn
+  handler          = "main.handler"
+  memory_size      = 128
+  timeout          = 60
   source_code_hash = data.archive_file.filter.output_base64sha256
-  runtime = "python3.8"
+  runtime          = "python3.8"
   environment {
     variables = {
       SLACK_URL     = var.slack_files_upload_endpoint_url
@@ -57,22 +57,22 @@ resource "aws_lambda_permission" "this" {
 resource "aws_cloudwatch_log_group" "this" {
   name              = "/aws/lambda/${aws_lambda_function.this.function_name}"
   retention_in_days = 30
-  tags = var.tags
+  tags              = var.tags
 }
 
 resource "aws_lambda_permission" "filter" {
-  statement_id  = "allow_cloudwatch_log_invoke_function"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.filter.function_name
-  principal     = "logs.region.amazonaws.com"
-  source_arn    =  "${data.aws_cloudwatch_log_group.this.arn}:"
+  statement_id   = "allow_cloudwatch_log_invoke_function"
+  action         = "lambda:InvokeFunction"
+  function_name  = aws_lambda_function.filter.function_name
+  principal      = "logs.region.amazonaws.com"
+  source_arn     = "${data.aws_cloudwatch_log_group.this.arn}:"
   source_account = data.aws_caller_identity.this.account_id
 }
 
-resource "aws_cloudwatch_log_group" "this" {
-  name              = "/aws/lambda/${aws_lambda_function.this.function_name}"
+resource "aws_cloudwatch_log_group" "filter" {
+  name              = "/aws/lambda/${aws_lambda_function.filter.function_name}"
   retention_in_days = 30
-  tags = var.tags
+  tags              = var.tags
 }
 
 
