7a39f5a3302c535221fc76d9c093551a08682e00
Vincent Donkor <vincent.donkor@gmail.com>
2022-12-21 19:45:04 -0500

    adding tw

--- python/tw/main.py
+++ python/tw/main.py
@@ -8,4 +8,21 @@ def get_tw_attachment():
     return [{"VpcId": tw["VpcId"], "SubnetIds": tw["SubnetIds"]} for tw in resp["TransitGatewayVpcAttachments"]]
 
 
+def get_subnets(vpc_id):
+    resp = ec2.describe_subnets(
+        Filters=[
+            {
+                "Name": "vpc-id",
+                "Values": [
+                    vpc_id
+                ]
+            },
+        ],
+    )
+    return [{"SubnetId": subnet["SubnetId"], "AvailabilityZoneId": subnet["AvailabilityZoneId"]} for subnet in
+            resp["Subnets"]]
+
+
 tw_details = get_tw_attachment()
+subnets = get_subnets(tw_details["VpcId"])
+print(subnets)
