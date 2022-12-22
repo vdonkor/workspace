23ad9593e043b2eb63e317b16bffff9ebcbd975b
Vincent Donkor <vincent.donkor@gmail.com>
2022-12-22 09:34:59 -0500

    adding subnet

--- python/tw/main.py
+++ python/tw/main.py
@@ -30,6 +30,13 @@ print(json.dumps(subnets, indent=3))
 
 
 tw_az_ids = [subnet["AvailabilityZoneId"] for subnet in subnets if subnet["SubnetId"] in tw_details[0]["SubnetIds"]]
-unique_subnet_az_ids = set([subnet["AvailabilityZoneId"] for subnet in subnets])
+unique_subnet_az_ids = list(set([subnet["AvailabilityZoneId"] for subnet in subnets]))
+
+if len(unique_subnet_az_ids) > len(tw_az_ids):
+    new_subnet_az_id = list(set(unique_subnet_az_ids) ^ set(tw_az_ids))
+    # get all subnets with the new az ids
+    unattached_subnet_ids = [subnet["SubnetId"] for subnet in subnets if subnet["AvailabilityZoneId"] in new_subnet_az_id]
+
+
 print(tw_az_ids)
 print(unique_subnet_az_ids)
